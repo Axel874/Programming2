2aac92a4ffbf2a0b21f7d92a9dab5fe2a21a8790
Axel874 <axel.avez@student.howest.be>
2021-02-25 15:41:36 +0100

    finished PowerUp 4.4

--- 1DAE08_MinGame_Avez_Axel/GD08MiniGameAvezAxel/PowerUps/Game.h
+++ 1DAE08_MinGame_Avez_Axel/GD08MiniGameAvezAxel/PowerUps/Game.h
@@ -1,11 +1,12 @@
 #pragma once
+#include "PowerUp.h"
 class Game final
 {
 public:
-	explicit Game( const Window& window );
+	explicit Game(const Window& window);
 	Game(const Game& other) = delete;
 	Game& operator=(const Game& other) = delete;
-	Game( Game&& other) = delete;
+	Game(Game&& other) = delete;
 	Game& operator=(Game&& other) = delete;
 	~Game();
 
@@ -23,8 +24,31 @@ private:
 	// DATA MEMBERS
 	const Window m_Window;
 
+	Point2f m_MousePos;
+	static const int m_NrPowerUps{ 10 };
+	PowerUp* m_pPowerUps[m_NrPowerUps];
+	Point2f m_PowerUpsCenters[m_NrPowerUps];
+	PowerUp::Type m_PowerUpTypes[m_NrPowerUps];
+	bool m_PowerUpsDestroyed[m_NrPowerUps];
+	float m_PowerUpRadius;
+	int m_ActNrPowerUps;
+	Rectf m_Destroyer;
+
 	// FUNCTIONS
 	void Initialize( );
 	void Cleanup( );
 	void ClearBackground( ) const;
+
+	void ShowTestMessage( ) const;
+	void ShowNrPowerUps( ) const;
+	void CreatePowerUps( );
+	void UpdatePowerUps( float elapsedSec );
+	void DrawPowerUps( ) const;
+	void DeletePowerUps( );
+
+	void MoveDestroyer( const Point2f& newCenter );
+	void DrawDestroyer( ) const;
+	void VerifyOverlapping( );
 };
+
+
